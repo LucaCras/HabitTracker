bc876c9874c6f92e1488f4efc8162393d1a74856
Luca Cras <Luca_Cras@hotmail.com>
2017-12-04 18:31:47 +0100

    Added alot of js for adding a habit. also updated more css and html

--- HabitTracker/js/app.js
+++ HabitTracker/js/app.js
@@ -1,8 +1,8 @@
-function Habit(name, duration, interval, good){
+function Habit(name, duration, days, type){
     this.name = name;
     this.duration = duration;
-    this.interval = interval;
-    this.good = good;
+    this.days = days;
+    this.type = type;
     this.succesful = 0;
     this.unsuccesful = 0;
 }
@@ -27,9 +27,37 @@ function HabitRabbit(name, color, avatar, lives) {
     this.lives = lives;
 }
 
-var habit = new Habit("Swimming", 69, 7, true);
+var main = function() {
 
-var bram = new HabitRabbit("bram", "blue", "hello", 3);
+  $("#add").click(function() {
+    document.getElementById('modal').style.display = 'block';
+  })
 
+  $(".close").click(function () {
+    document.getElementById('modal').style.display = 'none';
+  })  
 
-console.log(habit.unsuccesful);
+  $("#createhabit").submit(function(e) {
+    e.preventDefault();
+
+    var form = new FormData(document.getElementById("createhabit"));
+    var name = form.get("name");
+    var end = form.get("end");
+    var days = [];
+    var type = form.get("type");
+
+    $.each($("input[name='days']:checked"), function () {
+      days.push($(this).val());
+    });
+
+    var habit = new Habit(name, end, days, type);
+
+    console.log(habit);
+
+    $("#main ul").append('<li><div class="habit"><h2 class="habitname">' +habit.name + '</h2><p>' + habit.duration +' days remaining</p><p>'+ habit.days +'</p><p>'+ habit.type + '</p></div></li>');
+    document.getElementById('modal').style.display = 'none';
+  })
+}
+
+$(document).ready(main);
+ 
--- HabitTracker/js/index.js
+++ HabitTracker/js/index.js
@@ -2,6 +2,10 @@ var main = function(){
     $('#signup-form').submit(function (e) {
         window.alert("Successfully signed up!\n you can now log in :)");
     });
+    $('#login-form').submit(function (e) {
+        e.preventDefault();
+        window.location.href = "dashboard.html";
+    });
 }
 
 $(document).ready(main);
