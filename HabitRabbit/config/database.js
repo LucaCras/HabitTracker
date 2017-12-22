36f66a4a36e2886d4f42b4864f6dd2c08780adbf
Unknown <b.dikker@student.tudelft.nl>
2017-12-22 14:21:52 +0100

    Push up

---
+++ HabitRabbit/config/database.js
@@ -0,0 +1,18 @@
+/*************** MySQL configuratrion ********************/
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'konijn',
+    database: 'HabitRabbit'
+});
+
+connection.connect(err => {
+    if (err) {
+        throw err;
+    } else {
+        console.log("MySQL connected succesfully...")
+    }
+})
+
+module.exports = connection;
