135d301b4ce9a73a8a0ffb5697234c7124ea5eaf
Luca Cras <Luca_Cras@hotmail.com>
2017-12-22 12:03:24 +0100

    nieuwe HabitList Toegvoegd

--- HabitRabbit/public/js/dashboard.js
+++ HabitRabbit/public/js/dashboard.js
@@ -20,44 +20,45 @@ class Habit {
     }
 }
 
-var containsObject = function(obj, list) {
-  var i;
-  for (i = 0; i < list.length; i++) {
-      if (list[i] === obj) {
-          return true;
-      }
-  }
-  return false;
-}
-
-var createHTML = function(habit) {
-
-    return ('\
-      <li>\
-        <div class="habit" id="habit_' + habit.id + '">\
-          <a href="#" class="delete"  id="' + habit.id + '"><i class="fa fa-times" aria-hidden="true"></i></a>\
-          <div class="habit-content">\
-            <h2 class="habitname">' + habit.name + '</h2>\
-            <p>' + habit.duration + ' days remaining</p>\
-            <p>'+ habit.frequency + ' times a week</p>\
-            <p>'+ habit.good + '</p>\
-            <a href="#" id="edit"  id="' + habit.id + '"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>\
-          </div>\
-        </div>\
-      </li>'
-    );
-}
-
 var main = function() {
 
     var nextId = 0;
+    var habitList = [];
+
+    var insertIntoHabitList = function(habit) {
+        for (var i = 0; i < habitList.length; i++) {
+            if (habitList[i].id === habit.id) {
+                return null;
+            }
+        }
+        habitList.push(habit);
+    }
+
+    var createHTML = function(habit) {
+        instertIntoHabitList(rows[i]);
+        return ('\
+          <li>\
+            <div class="habit">\
+              <a href="#" class="delete"  id="' + habit.id + '"><i class="fa fa-times" aria-hidden="true"></i></a>\
+              <div class="habit-content">\
+                <h2 class="habitname">' + habit.name + '</h2>\
+                <p>' + habit.duration + ' days remaining</p>\
+                <p>'+ habit.frequency + ' times a week</p>\
+                <p>'+ habit.good + '</p>\
+                <a href="#" id="edit ' + habit.id + '"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>\
+              </div>\
+            </div>\
+          </li>'
+        );
+    }
 
     var getHabits = function(){
         $.getJSON('/dashboard/habits', (rows) => {
 
             var html = "";
-            amount = rows.length + 1;
+            nextId = rows.length + 1;
             for( var i = 0; i < rows.length; i++ ) {
+
                 habit = new Habit(rows[i].habit_id, rows[i].name, rows[i].duration, rows[i].frequency, rows[i].good);
                 html += createHTML(habit)
             }
@@ -69,57 +70,65 @@ var main = function() {
 
     setInterval(() => { getHabits() }, 10000)
 
-  $("#add").click(function() {
-      document.getElementById('modal').style.display = 'block';
-  })
+    $("#add").click(function() {
+        document.getElementById('modal').style.display = 'block';
+    })
 
-  $(".close").click(function () {
-      document.getElementById('modal').style.display = 'none';
-  })  
+    $(".close").click(function () {
+        document.getElementById('modal').style.display = 'none';
+    })  
 
-  $("#createhabit").submit(function(e) {
-      e.preventDefault();
+    $("#createhabit").submit(function(e) {
+        e.preventDefault();
 
-      var form = new FormData(document.getElementById("createhabit"));
-      var name = form.get("name");
-      var duration = form.get("duration");
-      var frequency = form.get("frequency");
-      var good;
-      if (form.get('type') == 'good') {
-          good = 1;
-      } else {
-          good = 0  ;
-      }
-
-
-      var newHabit = new Habit(nextId, name, duration, frequency, good)
-      $('#habits').append(createHTML(newHabit))
-
-      $.post('/dashboard/Luca/add', {name: name, duration: duration, frequency: frequency, good: good, user: 1})
+        var form = new FormData(document.getElementById("createhabit"));
+        var name = form.get("name");
+        var duration = form.get("duration");
+        var frequency = form.get("frequency");
+        var good;
+        if (form.get('type') == 'good') {
+            good = 1;
+        } else {
+            good = 0  ;
+        }
 
 
-      document.getElementById('modal').style.display = 'none';
-  })
+        var newHabit = new Habit(nextId, name, duration, frequency, good)
+        $('#habits').append(createHTML(newHabit))
 
-  $("#main ul").on("click",".delete", function() {
-    var id = this.id;
+        $.post('/dashboard/Luca/add', {name: name, duration: duration, frequency: frequency, good: good, user: 1})
 
-    $.post('/dashboard/Luca/delete', {id: id})
 
-    getHabits();
-  })
+        document.getElementById('modal').style.display = 'none';
+    })
 
-  $('#main ul ').change(function() {
-    //   var data = {
-    //       id: this.id,
-    //       name: this.name
-    //   }
+    $("#main ul").on("click",".delete", function() {
+        var id = this.id;
 
-    //   $.post()
-    //   console.log('edit')
-    console.log('hi')
-    console.log(this.parent)
-  })
+        $.post('/dashboard/Luca/delete', {id: id})
+
+        getHabits();
+    })
+
+    $("#main ul").on("click", ".#edit", function() {
+        //   var data = {
+        //       id: this.id,
+        //       name: this.name
+        //   }
+
+        //   $.post()
+        //   console.log('edit')
+        console.log(this.attr('')
+        $('[name="name"]').attr('value', $('')
+        document.getElementById('modal').style.display = 'block';
+
+    })
+
+    $("input").focus(function(){
+        $(this).once("click keyup", function(e){      
+            $(this).select();
+        });
+    });
 }
 
 
